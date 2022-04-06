55ecf9d7c9b42bdf2100d25b118b874fd966dcf9
ClayDK <William.clay.a@gmail.com>
2022-04-06 15:53:27 +0200

    50 procent til interaction

---
+++ Assets/custom code/interact.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interact : MonoBehaviour
+{
+    public GameObject playerobj;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (playerobj = null)
+        {
+            //playerobj = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        //RaycastHit2D hit = Physics2D.Raycast(playerobj, Vector2.zero);
+
+        //if (hit.collider != null)
+        {
+            //Debug.Log("Target name: " + hit.collider.name);
+        }
+    }
+}
